dfcd7e062b705de13a51578d20677ea1a638223e
Shane Kelly <shanekweb@icloud.com>
2016-03-15 13:52:27 +0000

    highlights

--- app/controllers/spree/products_controller.rb
+++ app/controllers/spree/products_controller.rb
@@ -14,7 +14,7 @@ module Spree
       @taxonomies = Spree::Taxonomy.includes(root: :children)
 
       @query = params[:q].presence || "*"
-      @products = Spree::Product.search(@query, fields: [:name, :machine_models], highlight: {fields: [:name]})
+      @products = Spree::Product.search(@query, fields: [:name, :machine_models], highlight: {fields: [:name]}).upcase
     end
 
     def show
